3d63fe1e448d583b77fb8c6a6ac224acb582382c
2DAW <github.goatskin743@passinbox.com>
2025-10-06 11:01:42 +0200

    Cambiando el titulo en h1

--- elementos/header.php
+++ elementos/header.php
@@ -2,6 +2,7 @@
 
   <!-- barra de navegacion con boostrap -->   
 
+  
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Barra de navegación</a>
@@ -17,6 +18,6 @@
   </div>
 </nav>
 
-  <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
+  <h1 class="text-primary">🌟 Bienvenido a mi primera pagina web 🌟</h1>
   <p class="text-muted">Usando include() por primera vez</p>
 </header>
