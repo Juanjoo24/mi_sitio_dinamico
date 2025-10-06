0d4f9d4dc73d0206e74e6e781ff6cb8224c58659
2DAW <github.goatskin743@passinbox.com>
2025-10-06 10:30:51 +0200

    Añadida barra de navegación Bootstrap

--- elementos/header.php
+++ elementos/header.php
@@ -1,4 +1,22 @@
 <header class="text-center bg-white p-3 rounded shadow-sm">
+
+  <!-- barra de navegacion con boostrap -->   
+
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Barra de navegación</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="#">Productos</a>
+        <a class="nav-link" href="#">Precios</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
   <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
   <p class="text-muted">Usando include() por primera vez</p>
 </header>
